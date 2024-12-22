ec2b994f20cc5e327638346bb907e3d3ab01d229
sagar kumar <sagarkumar020599@gmail.com>
2024-12-22 09:54:41 +0530

    Dynamic array implementation

--- DynamicArray.c
+++ DynamicArray.c
@@ -16,6 +16,23 @@ struct DynamicArray* createDynamicArray(int cap)
     return darr;
 
 }
+
+void delete(struct DynamicArray* darr,int index)
+{
+    if(index>darr->lastIndex||index>darr->capacity||index<0)
+    { 
+        printf("Invalid index");
+       
+
+    }
+    else{
+     for(int i=index;i<darr->lastIndex;i++)
+        {
+            darr->ptr[i]=darr->ptr[i+1];
+        }
+        darr->lastIndex--;
+    }
+}
 void insertValue(struct DynamicArray* darr,int value)
 {
     // if(darr->lastIndex>darr->capacity-1)
@@ -28,38 +45,47 @@ void insertValue(struct DynamicArray* darr,int value)
     // }
 
 }
+void delteFromlast(struct DynamicArray* darr)
+{
+    if(darr->lastIndex<0)
+    {
+        printf("Array is empty not have element to delete\n");
+    }
+    else{
+        darr->lastIndex--;
+    }
+}
 void updateSize(struct DynamicArray* darr,int size)
 { 
-    struct DynamicArray* temp;
-      temp=(struct DynamicArray*)malloc(sizeof(struct DynamicArray));
-      temp->ptr=(int*)malloc(sizeof(int)*size);
-      darr->capacity=size;
-      for(int i=0;i<=darr->lastIndex;i++)
-      {
-        temp->ptr[i]=darr->ptr[i];
-      }
-      free(temp);
+    int *p=darr->ptr;
+    darr->ptr=(int *)malloc(sizeof(int)*size);
+    for(int i=0;p[i];i++)
+    {
+        darr->ptr[i]=p[i];
+    }
+    darr->capacity=size;
+    
+     
 }
 int main(int argc, char const *argv[])
 {
+    int a[3];
+    a[56]=445;
     struct DynamicArray* darr;
     darr=createDynamicArray(3);
     insertValue(darr,45);
     insertValue(darr,46);
-    int arr[2];
-    insertValue(darr,345);
-    insertValue(darr,88);
-    insertValue(darr,85); 
-
-
-   
-    updateSize(darr,5);
-
+    insertValue(darr,45);
+    insertValue(darr,25);
+    insertValue(darr,73);
+    delteFromlast(darr);
     printf("size: %d\n",darr->capacity);
     for(int i=0;i<=darr->lastIndex;i++)
     {
       printf("%d ", darr->ptr[i]);
     }
+    free(darr->ptr);
+    free(darr);
    
     return 0;
 }
